04cc0f840145a8ee9833422b9671e56db91c4163
Omar Orozco <ing.omar.orozco@gmail.com>
2018-07-26 18:23:05 -0400

    cambio de libreria

--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -46,8 +46,8 @@ if ($lang) {
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/translatable.css?9ae093d"/>
 
     <!-- Modificaciones de CC and CCO-->   
-    <script type="text/javascript" src="<?php echo ROOT_PATH ?>scp/selectize.js/selectize.js"></script>
-    <link rel="stylesheet" type="text/css" href="<?php echo ROOT_PATH ?>scp/selectize.js/selectize.css" />
+    <script type="text/javascript" src="<?php echo ROOT_PATH ?>scp/selectize.js/dist/js/selectize.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo ROOT_PATH ?>scp/selectize.js/dist/css/selectize.css" />
     
 
     
