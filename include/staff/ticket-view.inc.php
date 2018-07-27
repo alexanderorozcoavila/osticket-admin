4903ebe660878593b597c56c147d37016a488fe3
Omar Orozco <ing.omar.orozco@gmail.com>
2018-07-26 20:50:46 -0400

    cambio de libreria

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -581,45 +581,74 @@ if ($errors['err'] && isset($_POST['a'])) {
                 <td>
                     <select id="select-to" class="contacts" placeholder="Pick some people..."></select>
                     <script>
-                    ('#select-to').selectize({
-                        valueField: 'url',
+                    var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
+                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
+
+                    $('#select-to').selectize({
+                        persist: false,
+                        maxItems: null,
+                        valueField: 'email',
                         labelField: 'name',
-                        searchField: 'name',
-                        create: false,
-                        render: {
-                            option: function(item, escape) {
-                                return '<div>' +
-                                    '<span class="title">' +
-                                        '<span class="name"><i class="icon ' + (item.fork ? 'fork' : 'source') + '"></i>' + escape(item.name) + '</span>' +
-                                        '<span class="by">' + escape(item.username) + '</span>' +
-                                    '</span>' +
-                                    '<span class="description">' + escape(item.description) + '</span>' +
-                                    '<ul class="meta">' +
-                                        (item.language ? '<li class="language">' + escape(item.language) + '</li>' : '') +
-                                        '<li class="watchers"><span>' + escape(item.watchers) + '</span> watchers</li>' +
-                                        '<li class="forks"><span>' + escape(item.forks) + '</span> forks</li>' +
-                                    '</ul>' +
-                                '</div>';
-                            }
-                        },
-                        score: function(search) {
-                            var score = this.getScoreFunction(search);
-                            return function(item) {
-                                return score(item) * (1 + Math.min(item.watchers / 100, 1));
-                            };
-                        },
+                        searchField: ['name', 'email'],
+                        options: [],
                         load: function(query, callback) {
                             if (!query.length) return callback();
                             $.ajax({
-                                url: 'https://api.github.com/legacy/repos/search/' + encodeURIComponent(query),
+                                url: 'ajax.php/users/local?q=' + encodeURIComponent(query),
                                 type: 'GET',
                                 error: function() {
                                     callback();
                                 },
                                 success: function(res) {
-                                    callback(res.repositories.slice(0, 10));
+                                    console.log(res);
+                                    callback(res);
                                 }
                             });
+                        },
+                        render: {
+                            item: function(item, escape) {
+                                return '<div>' +
+                                    (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+                                    (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                                '</div>';
+                            },
+                            option: function(item, escape) {
+                                var label = item.name || item.email;
+                                var caption = item.name ? item.email : null;
+                                return '<div>' +
+                                    '<span class="label">' + escape(label) + '</span>' +
+                                    (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+                                '</div>';
+                            }
+                        },
+                        createFilter: function(input) {
+                            var match, regex;
+
+                            // email@address.com
+                            regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[0]);
+
+                            // name <email@address.com>
+                            regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[2]);
+
+                            return false;
+                        },
+                        create: function(input) {
+                            if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
+                                return {email: input};
+                            }
+                            var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
+                            if (match) {
+                                return {
+                                    email : match[2],
+                                    name  : $.trim(match[1])
+                                };
+                            }
+                            alert('Invalid email address.');
+                            return false;
                         }
                     });
                     </script>
