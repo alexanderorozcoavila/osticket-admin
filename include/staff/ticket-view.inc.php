e4db8d3af092f563127d19cb2703bfaed50a28ea
Omar Orozco <ing.omar.orozco@gmail.com>
2018-07-26 21:00:02 -0400

    cambio de libreria

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -601,54 +601,9 @@ if ($errors['err'] && isset($_POST['a'])) {
                                 },
                                 success: function(res) {
                                     console.log(res);
-                                    callback(res.slice(0, 10));
+                                    callback(res);
                                 }
                             });
-                        },
-                        render: {
-                            item: function(item, escape) {
-                                return '<div>' +
-                                    (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
-                                    (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
-                                '</div>';
-                            },
-                            option: function(item, escape) {
-                                var label = item.name || item.email;
-                                var caption = item.name ? item.email : null;
-                                return '<div>' +
-                                    '<span class="label">' + escape(label) + '</span>' +
-                                    (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
-                                '</div>';
-                            }
-                        },
-                        createFilter: function(input) {
-                            var match, regex;
-
-                            // email@address.com
-                            regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
-                            match = input.match(regex);
-                            if (match) return !this.options.hasOwnProperty(match[0]);
-
-                            // name <email@address.com>
-                            regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
-                            match = input.match(regex);
-                            if (match) return !this.options.hasOwnProperty(match[2]);
-
-                            return false;
-                        },
-                        create: function(input) {
-                            if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
-                                return {email: input};
-                            }
-                            var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
-                            if (match) {
-                                return {
-                                    email : match[2],
-                                    name  : $.trim(match[1])
-                                };
-                            }
-                            alert('Invalid email address.');
-                            return false;
                         }
                     });
                     </script>
