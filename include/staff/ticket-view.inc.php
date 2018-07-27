94c96b2c29b34cf58d5b49a534e0b80029b44dee
Omar Orozco <ing.omar.orozco@gmail.com>
2018-07-26 20:44:59 -0400

    cambio de libreria

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -582,19 +582,23 @@ if ($errors['err'] && isset($_POST['a'])) {
                     <select id="select-to" class="contacts" placeholder="Pick some people..."></select>
                     <script>
                     $('#select-to').selectize({
-                        persist: false,
-                        maxItems: null,
-                        valueField: 'email',
+                        valueField: 'url',
                         labelField: 'name',
-                        searchField: ['name', 'email'],
+                        searchField: 'name',
                         create: false,
                         render: {
                             option: function(item, escape) {
-                                var label = item.name || item.email;
-                                var caption = item.name ? item.email : null;
                                 return '<div>' +
-                                    '<span class="label">' + escape(label) + '</span>' +
-                                    (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+                                    '<span class="title">' +
+                                        '<span class="name"><i class="icon ' + (item.fork ? 'fork' : 'source') + '"></i>' + escape(item.name) + '</span>' +
+                                        '<span class="by">' + escape(item.username) + '</span>' +
+                                    '</span>' +
+                                    '<span class="description">' + escape(item.description) + '</span>' +
+                                    '<ul class="meta">' +
+                                        (item.language ? '<li class="language">' + escape(item.language) + '</li>' : '') +
+                                        '<li class="watchers"><span>' + escape(item.watchers) + '</span> watchers</li>' +
+                                        '<li class="forks"><span>' + escape(item.forks) + '</span> forks</li>' +
+                                    '</ul>' +
                                 '</div>';
                             }
                         },
