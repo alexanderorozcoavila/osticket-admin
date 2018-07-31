42d28e7815cf28407f2f88c610e5eff715124278
Omar Orozco <ing.omar.orozco@gmail.com>
2018-07-31 17:45:56 -0400

    correccion ocultar cco

--- include/staff/ticket-view.inc.php
+++ include/staff/ticket-view.inc.php
@@ -1227,7 +1227,7 @@ $(function() {
 
         <?php if ($colaboradores2 == "" ){ ?>
         $( document ).ready(function() {
-            $("#tr-cco").hide()
+            $("#tr-cco").css('display':'none');
         });
         <?php } ?>
 
