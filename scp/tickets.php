f2467f0f3c5bf5322280d34bbda927fe4fe9de73
Omar Orozco <ing.omar.orozco@gmail.com>
2018-08-31 01:15:39 -0400

    conflicto editar

--- scp/tickets.php
+++ scp/tickets.php
@@ -461,7 +461,7 @@ if($ticket) {
         
         //print "editando";
         //$ticket->logConflictTikcet();
-        $th = $ticket->getThread();
+        $th = $ticket->getThread()->getEvents();
         print var_dump($th);
         exit;
         $inc = 'ticket-edit.inc.php';
