059550e9b7c8aec13b1d8e06a8ee9c78d33024c2
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-02 11:31:37 -0400

    conflicto editar

--- include/staff/ticket-open.inc.php
+++ include/staff/ticket-open.inc.php
@@ -195,6 +195,245 @@ if ($_POST)
                 <div class="error"><?php echo $errors['name']; ?></div>
             </td>
         </tr>
+        <tr>
+                <td width="120">
+                    <label><strong><?php echo __('CC'); ?>:</strong></label>
+                </td>
+                <td>
+                    <input type='checkbox' value='1' name="emailcollab"
+                    id="t<?php echo $ticket->getThreadId(); ?>-emailcollab"
+                        <?php echo ((!$info['emailcollab'] && !$errors) || isset($info['emailcollab']))?'checked="checked"':''; ?>
+                        style="display:none;"
+                        >
+                        
+                    <div style="
+                    float:left;
+                    border: 1px solid #d0d0d0;
+                    padding: 10px 10px;
+                    width: 100%;
+                    background: #fff;
+                    -webkit-box-sizing: border-box;
+                    -moz-box-sizing: border-box;
+                    box-sizing: border-box;
+                    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1);
+                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1);
+                    -webkit-border-radius: 3px;
+                    -moz-border-radius: 3px;
+                    border-radius: 3px;">
+                    <select id="cc-colaboradores" class="contacts" placeholder="Agregar"  multiple style="width: 90%;">
+                        
+                    </select>
+                    <span style="
+                    float: right;
+                    top: 0px;
+                    right: 0px;
+                    margin-right: 10px;
+                    margin-top: -27px;cursor:pointer;" id="span-cco">CCO</span>
+                    </div>
+                    <script>
+                    var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
+                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
+
+                    $('#cc-colaboradores').selectize({
+                        persist: true,
+                        maxItems: null,
+                        plugins: ['remove_button'],
+                        valueField: 'id',
+                        labelField: 'email',
+                        searchField: ['name', 'email','phone'],
+                        options: [],
+                        load: function(query, callback) {
+                            if (!query.length) return callback();
+                            $.ajax({
+                                url: 'ajax.php/users/local?q=' + encodeURIComponent(query),
+                                type: 'GET',
+                                // async:false,
+                                dataType: 'json',
+                                error: function() {
+                                    callback();
+                                },
+                                success: function(res) {
+                                    console.log(res);
+                                    callback(res);
+                                }
+                            });
+                        },
+                        render: {
+                            item: function(item, escape) {
+                                //console.log(item.name + '1');
+                                return '<div>' +
+                                    (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                                '</div>';
+
+                            },
+                            option_create: function(item, escape) {
+                                return '<div class="create">Agregar <strong>' + escape(item.input) + '</strong>&hellip;</div>';
+                            }
+                        },
+                        createFilter: function(input) {
+                            var match, regex;
+
+                            // email@address.com
+                            regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[0]);
+
+                            // name <email@address.com>
+                            regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[2]);
+
+                            return false;
+                        },
+                        
+                        create: function(input) {
+                            
+                            if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
+                                idusernew = 0;
+                                $.ajax({
+                                    url: 'ajax.php/ccandcco/<?php echo $ticket->getThreadId(); ?>/adduser',
+                                    type: 'POST',
+                                    async:false,
+                                    data: { name: input, email:input },
+                                    // dataType: 'json',
+                                    error: function() {
+                                        console.log('error');
+                                    },
+                                    success: function(res) {
+                                        idusernew = res;
+                                        
+                                    }
+                                }); 
+                                return {email: input, id:idusernew}; 
+                                
+                            }
+                            var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
+                            if (match) {
+                                return {
+                                    email : match[2],
+                                    name  : $.trim(match[1])
+                                };
+                            }
+                            alert('Invalid email address.');
+                            return false;
+                        }
+                    });
+                    </script>
+                </td>
+             </tr>
+                
+             <tr id="tr-cco" style="display:none;">
+                <td width="120">
+                    <label><strong><?php echo __('CCO'); ?>:</strong></label>
+                </td>
+                <td>
+                <div style="
+                    float:left;
+                    border: 1px solid #d0d0d0;
+                    padding: 10px 10px;
+                    width: 100%;
+                    background: #fff;
+                    -webkit-box-sizing: border-box;
+                    -moz-box-sizing: border-box;
+                    box-sizing: border-box;
+                    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1);
+                    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.1);
+                    -webkit-border-radius: 3px;
+                    -moz-border-radius: 3px;
+                    border-radius: 3px;">
+                <select id="cco-colaboradores" class="contacts" placeholder="Agregar" multiple style="width: 90%;">
+                
+                </select>
+                </div>
+                    <script>
+                    var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
+                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
+
+                    $('#cco-colaboradores').selectize({
+                        persist: true,
+                        maxItems: null,
+                        valueField: 'id',
+                        labelField: 'email',
+                        plugins: ['remove_button'],
+                        searchField: ['name', 'email','phone'],
+                        options: [],
+                        load: function(query, callback) {
+                            if (!query.length) return callback();
+                            $.ajax({
+                                url: 'ajax.php/users/local?q=' + encodeURIComponent(query),
+                                type: 'GET',
+                                // async:false,
+                                dataType: 'json',
+                                error: function() {
+                                    callback();
+                                },
+                                success: function(res) {
+                                    console.log(res);
+                                    callback(res);
+                                }
+                            });
+                        },
+                        render: {
+                            item: function(item, escape) {
+                                //console.log(item.name + '1');
+                                return '<div>' +
+                                    (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                                '</div>';
+                            },
+                            option_create: function(item, escape) {
+                                return '<div class="create">Agregar <strong>' + escape(item.input) + '</strong>&hellip;</div>';
+                            }
+                        },
+                        createFilter: function(input) {
+                            var match, regex;
+
+                            // email@address.com
+                            regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[0]);
+
+                            // name <email@address.com>
+                            regex = new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i');
+                            match = input.match(regex);
+                            if (match) return !this.options.hasOwnProperty(match[2]);
+
+                            return false;
+                        },
+                        create: function(input) {
+                            
+                            if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
+                                idusernew = 0;
+                                $.ajax({
+                                    url: 'ajax.php/ccandcco/<?php echo $ticket->getThreadId(); ?>/adduser',
+                                    type: 'POST',
+                                    async:false,
+                                    data: { name: input, email:input },
+                                    // dataType: 'json',
+                                    error: function() {
+                                        console.log('error');
+                                    },
+                                    success: function(res) {
+                                        idusernew = res;
+                                        
+                                    }
+                                }); 
+                                return {email: input, id:idusernew}; 
+                                
+                            }
+                            var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
+                            if (match) {
+                                return {
+                                    email : match[2],
+                                    name  : $.trim(match[1])
+                                };
+                            }
+                            alert('Invalid email address.');
+                            return false;
+                        }
+                    });
+                    </script>
+                </td>
+             </tr>
         <?php
         } ?>
 
@@ -432,6 +671,14 @@ print $response_form->getField('attachments')->render();
 </p>
 </form>
 <script type="text/javascript">
+$('#span-cco').click(function() {
+            if ($("#tr-cco").css('display') == 'none') {
+                $("#tr-cco").css('display','table-row');
+            }else{
+                $("#tr-cco").css('display','none');
+            }
+            console.log('open tr');
+        });
 $(function() {
     $('#new-ticket').click(function(){
         var opcion = confirm("¿Seguro que desea salir? \nCualquier cambio o información que hayas introducido \nserán descartados");
