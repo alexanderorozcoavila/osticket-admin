3db992e37546ba8429790b2ad5d34c76c16bcbf0
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-06 00:26:51 -0400

    conflicto ticket

---
+++ updatethreadevent.php
@@ -0,0 +1,14 @@
+<?php
+// $link = mysqli_connect("localhost", "administrador", "BtCFfa~G5n=9", "nous00_os1");
+                        
+// if ($link == false) {
+//     die("ERROR: Could not connect. "
+//                 .mysqli_connect_error());
+// }
+
+// $sql = "SET NAMES 'utf8'";
+// mysqli_query($link, $sql);
+$date = date('Y-m-d H:i:s');
+echo $date;
+
+?>
