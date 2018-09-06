c45969e92b67886e4ca20a6a880c87fa499eb5fe
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-06 00:52:36 -0400

    conflicto ticket

--- updatethreadevent.php
+++ updatethreadevent.php
@@ -14,8 +14,10 @@ while ($row = mysqli_fetch_array($res)) {
     $hora_actual = strtotime($row['hora_actual']);
     if($hora_actual >= $hora_limite){
         echo "liberar";
+        echo "\n";
     }else{
         echo "no liberar";
+        echo "\n";
     }
 
 }
