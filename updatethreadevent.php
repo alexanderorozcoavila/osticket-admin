fedc521fee0e17e54cc2d03ea2ea9368c91356eb
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-06 00:50:33 -0400

    conflicto ticket

--- updatethreadevent.php
+++ updatethreadevent.php
@@ -1,14 +1,23 @@
 <?php
-// $link = mysqli_connect("localhost", "administrador", "BtCFfa~G5n=9", "nous00_os1");
+$link = mysqli_connect("localhost", "administrador", "BtCFfa~G5n=9", "nous00_os1");
                         
-// if ($link == false) {
-//     die("ERROR: Could not connect. "
-//                 .mysqli_connect_error());
-// }
+if ($link == false) {
+    die("ERROR: Could not connect. "
+                .mysqli_connect_error());
+}
 
-// $sql = "SET NAMES 'utf8'";
-// mysqli_query($link, $sql);
-$date = date('Y-m-d H:i:s');
-echo $date;
+$sql = "SELECT timestamp as hora, DATE_ADD(timestamp, INTERVAL 5 MINUTE) as limite, NOW() as hora_actual FROM `os_thread_event` WHERE `data` = 'notedit'";
+$res = mysqli_query($link, $sql);
+while ($row = mysqli_fetch_array($res)) {
+    $hora_thread = strtotime($row['hora']);
+    $hora_limite = strtotime($row['limite']);
+    $hora_actual = strtotime($row['hora_actual']);
+    if($hora_actual >= $hora_limite){
+        echo "liberar";
+    }else{
+        echo "no liberar";
+    }
+
+}
 
 ?>
