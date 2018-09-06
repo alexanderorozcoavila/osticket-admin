9ba1fa33e4faaadbf0166f0eb0473cdb90d1a2d9
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-06 01:05:25 -0400

    conflicto ticket

--- updatethreadevent.php
+++ updatethreadevent.php
@@ -6,17 +6,24 @@ if ($link == false) {
                 .mysqli_connect_error());
 }
 
-$sql = "SELECT timestamp as hora, DATE_ADD(timestamp, INTERVAL 5 MINUTE) as limite, NOW() as hora_actual FROM `os_thread_event` WHERE `data` = 'notedit'";
+$sql = "SELECT id, thread_id, timestamp as hora, DATE_ADD(timestamp, INTERVAL 5 MINUTE) as limite, NOW() as hora_actual FROM `os_thread_event` WHERE `data` = 'notedit'";
 $res = mysqli_query($link, $sql);
 while ($row = mysqli_fetch_array($res)) {
     $hora_thread = strtotime($row['hora']);
     $hora_limite = strtotime($row['limite']);
     $hora_actual = strtotime($row['hora_actual']);
     if($hora_actual >= $hora_limite){
-        echo "liberar";
-        echo "\n";
+        $sql2 ="DELETE FROM `os_thread_event` WHERE `os_thread_event`.`id` = ".$row['id'];
+        $resultado = mysqli_query($link, $sql2);
+        if($resultado){
+            echo $row['thread_id'].": liberado...";
+            echo "\n";
+        }else{
+            echo $row['thread_id'].": en espera...";
+            echo "\n";
+        }
     }else{
-        echo "no liberar";
+        echo $row['thread_id'].": en espera...";
         echo "\n";
     }
 
